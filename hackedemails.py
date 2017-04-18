1a2c1f3623dc648a33388cec0b1270179d2bb786
Maxavier Jeanphilippe <mjeanphilippe@gvit.com>
2017-04-18 17:10:21 -0400

    Update hackedemails.py
    added argument parser

--- hackedemails.py
+++ hackedemails.py
@@ -8,21 +8,31 @@ from argparse import ArgumentParser
 class HackedEmailstoDradis(object):
     def __init__(self):
         self.arg = self.parse_args()
+        print(len(argv))
+        if len(argv) < 5:
+            print("wrong argument amount. see HELP")
+            exit(-5)
+        if ".csv" not in self.arg.CompanyID_or_CSVFilename:
+            if len(argv) != 13:
+                print("wrong argument amount for ConnectWise. see HELP")
+                exit(-13)
         self.emails = []
         self.apihitcounter = 0
         self.session = Session()
-
+        self.verify_cert = True  # change this to make requests without verifying
         # ConnectWise API Configuration
-        self.connectwise_company_name = "xxxxxxxxxx"
-        self.connectwise_public_api_key = "xxxxxxxxxxxx"
-        self.connectwise_private_api_key = "xxxxxxxxxxx"
-        self.connectwise_api_site = "xxxxxxxxxxx"
-        self.connectwise_all_companies_site = self.connectwise_api_site +  '/company/contacts?pageSize=1000'
-        self.connectwise_one_company_site = self.connectwise_api_site + '/company/contacts?conditions=company/identifier like "{0}"& pageSize=1000'
+        self.connectwise_company_name = self.arg.connectwise_company_name
+        self.connectwise_public_api_key = self.arg.public_api_key
+        self.connectwise_private_api_key = self.arg.connectwise_private_api_key
+        self.connectwise_api_site = self.arg.connectwise_api_site
+        self.connectwise_all_companies_site = self.connectwise_api_site + '/company/contacts?pageSize=1000'
+        self.connectwise_one_company_site = self.connectwise_api_site + '/company/contacts?conditions=company/' \
+                                                                        'identifier like "{0}"& pageSize=1000'
         # Dradis API Configuration
-        self.dradis_api_token = 'xxxxxxxxxxxxxxx'
-        self.dradis_project_id = '0'
-        self.dradis_issues_url = 'https://dradis-pro.dev/pro/api/issues'
+        self.dradis_api_token = self.arg.dradis_api_token
+        self.dradis_project_id = self.arg.dradis_project_id
+        self.dradis_url = self.arg.dradis_url
+        self.dradis_issues_url = '{0}/pro/api/issues/'.format(self.dradis_url)
 
     def run(self):
         self.emails = self.get_emails()
@@ -48,7 +58,7 @@ class HackedEmailstoDradis(object):
                 exit(-1)
             return contacts
         elif self.arg.CompanyID_or_CSVFilename == 'all':
-            self.session.auth = (self.connectwise_company_name  + '+{0}'.format(self.connectwise_public_api_key),
+            self.session.auth = (self.connectwise_company_name + '+{0}'.format(self.connectwise_public_api_key),
                                  self.connectwise_private_api_key)
             contacts = self.session.get(self.connectwise_all_companies_site)
             if contacts.status_code != 200:
@@ -57,7 +67,7 @@ class HackedEmailstoDradis(object):
             self.session.auth = None
             return contacts.json()
         else:
-            self.session.auth = (self.connectwise_company_name  + '+{0}'.format(self.connectwise_public_api_key),
+            self.session.auth = (self.connectwise_company_name + '+{0}'.format(self.connectwise_public_api_key),
                                  self.connectwise_private_api_key)
             contacts = self.session.get(self.connectwise_one_company_site.format(self.arg.CompanyID_or_CSVFilename))
             if contacts.status_code != 200:
@@ -70,13 +80,14 @@ class HackedEmailstoDradis(object):
         for email in emails:
             if self.apihitcounter == 100:
                     print("The hacked-emails API limit is 100. The script will now exit.")
-                    exit(-1) 
+                    exit(-1)
             hacked_email = self.session.get("https://hacked-emails.com/api?q={0}".format(email))
             self.apihitcounter += 1
             if hacked_email.status_code != 200:
                 print(email + hacked_email.text)
                 continue
             elif hacked_email.json()['status'] == "notfound":
+                print(email + 'has no entries in hackedemails.com')
                 continue
             else:
                 self.session.headers.update({'Authorization': 'Token token="{0}"'.format(self.dradis_api_token)})
@@ -90,8 +101,11 @@ class HackedEmailstoDradis(object):
                 data = {'issue': {
                         'text': '#[Title]#\r\n' + email + ' - hacked-emails.com\r\n\r\n#[Results]#\r\n' + str(
                             hacked_email.json()['results']) + "\r\n\r\n" + _data + "\r\n\r\n"}}
-                dradis = self.session.post(self.dradis_issues_url, data=dumps(data), verify=False)
-                print(email + ' ' + str(dradis.status_code))
+                dradis = self.session.post(self.dradis_issues_url, data=dumps(data), verify=self.verify_cert)
+                if dradis.status_code == 201:
+                    print(email + ' was imported into Dradis')
+                else:
+                    print(email + ' was not imported into Dradis: ' + dradis.text)
                 self.session.headers.clear()
 
     def connectwise_hacked_emails_to_dradis(self, emails):
@@ -112,6 +126,7 @@ class HackedEmailstoDradis(object):
                     print(email + hacked_email.text)
                     continue
                 elif hacked_email.json()['status'] == "notfound":
+                    print(email + 'has no entries in hackedemails.com')
                     continue
                 else:
                     self.session.headers.update({'Authorization': 'Token token="{0}"'.format(self.dradis_api_token)})
@@ -126,19 +141,30 @@ class HackedEmailstoDradis(object):
                     data = {'issue': {
                         'text': '#[Title]#\r\n' + email + ' - hacked-emails.com\r\n\r\n#[Results]#\r\n' + str(
                             hacked_email.json()['results']) + "\r\n\r\n" + _data + "\r\n\r\n"}}
-                    dradis = self.session.post(self.dradis_issues_url, data=dumps(data), verify=False)
-                    print(email + ' - ' + str(dradis.status_code))
+                    dradis = self.session.post(self.dradis_issues_url, data=dumps(data), verify= self.verify_cert)
+                    if dradis.status_code == 201:
+                        print(email + ' was imported into Dradis')
+                    else:
+                        print(email + ' was not imported into Dradis: ' + dradis.text)
                     self.session.headers.clear()
             else:
                 continue
 
     def parse_args(self):
         # parse the arguments
-        parser = ArgumentParser(epilog='\tExample: \r\npython ' + argv[0] + " -CompanyID_or_CSVFilename",
-                                         description="Check contacts' or csv of emails in the hacked-emails API and "
+        parser = ArgumentParser(epilog='\tCSV Example: \r\npython ' + argv[0] + "CompanyID_or_CSVFilename " + "Dradis_URL Dradis_Project_ID Dradis_API_Token\r\n\tConnectWise Example: \r\npython "+ argv[0] + "-c GoVanguard -s https://connectwiseapisite.com -u 12123 -p 41424124",
+                                         description="Check ConnectWise Company Contacts' or csv of emails in the hacked-emails API and "
                                                      "export the results to Dradis as issues")
         parser._optionals.title = "OPTIONS"
+        parser.add_argument('-c', dest='connectwise_company_name', help="Your Company Name is ConnectWIse", required=False)
+        parser.add_argument('-s', dest='connectwise_api_site', help="Your ConnectWise API site", required=False)
+        parser.add_argument('-u', dest='connectwise_public_api_key',help="ConnectWise Public API Key", required=False)
+        parser.add_argument('-p', dest='connectwise_private_api_key', help="ConnectWise Private API Key", required=False)
+
         parser.add_argument('CompanyID_or_CSVFilename', help="ConnectWise Company ID or CSV of emails")
+        parser.add_argument('dradis__url', help="Dradis URL")
+        parser.add_argument('dradis_project_id', help="Dradis Project ID")
+        parser.add_argument('dradis_api_token', help="Dradis API Token")
         return parser.parse_args()
 
 if __name__ == "__main__":
